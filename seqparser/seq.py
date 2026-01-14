01c724ad58246ad4c3eb537e95e231f407723f98
Runa Cheng <rcheng@RC.ucsf.edu>
2026-01-13 21:20:00 -0800

    completed seq.y

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -10,13 +10,20 @@ def transcribe(seq: str, reverse: bool = False) -> str:
     Write a function that will transcribe (replace DNA sequence to RNA
     by replacing all 'T' to 'U') in an input sequence
     """
-    pass
+    return seq.replace("T", "U")
 
 def reverse_transcribe(seq: str) -> str:
     """
     Write a function that will transcribe an input sequence and reverse
     the sequence
     """
+    reverseTranscript = ""
+
+    for bp in seq:
+        reverseTranscript += mapping.get(bp, bp)
+    
+    return reverseTranscript[::-1]
+        
     # Hey this is my comment
     # Again!
     pass
