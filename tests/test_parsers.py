df62969a39ab2f3ab19235937e642f2c21846812
Runayh-cheng <yan.cheng@ucsf.edu>
2026-01-15 22:55:14 -0800

    path update

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -33,8 +33,8 @@ def test_FastaParser():
     files that are blank or corrupted in some way. Two example Fasta files are
     provided in /tests/bad.fa and /tests/empty.fa
     """
-    assert list(FastaParser(blank.fa)) == []
-    assert FastaParser(bad.fa) == []
+    assert list(FastaParser("tests/blank.fa")) == []
+    assert FastaParser("tests/bad.fa"") == []
     assert len(list((FastaParser("data/test.fa")))) == 100
 
 
